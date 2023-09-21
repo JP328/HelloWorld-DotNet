e8288a219c03b881f39b6365c066c734b2700b66
João Pedro <joaopedrodamaiamoura@outlook.com>
2023-09-21 17:02:35 -0300

    Hello World

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace helloworld.Controllers
+{
+    [Route("~/")]        
+    [ApiController]
+    public class HelloController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello World - .Net is LIVE!";
+        }
+
+        [HttpGet("/meunome")]
+        public string Nome()
+        {
+            return "Joãozinho gameplay!";
+        }
+    }
+}
