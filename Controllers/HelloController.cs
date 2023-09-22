66b6d1b10f66b965502e5fb2f906f09699b0cfae
João Pedro <joaopedrodamaiamoura@outlook.com>
2023-09-22 17:48:43 -0300

    Atualizando conteúdo

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -12,10 +12,24 @@ namespace helloworld.Controllers
             return "Hello World - .Net is LIVE!";
         }
 
-        [HttpGet("/meunome")]
-        public string Nome()
+        [HttpGet("/bsms")]
+        public string Bsms()
         {
-            return "Joãozinho gameplay!";
+            return "Mentalidade de crescimento\n" +
+                    "Orientação ao futuro\n" +
+                    "Orientação ao detalhe\n" +
+                    "Comunicação\n" +
+                    "Proatividade\n" +
+                    "Persistência\n" +
+                    "Trabalho em Equipe\n" +
+                    "Responsabilidade Pessoal";
+        }
+
+
+        [HttpGet("/metas-da-semana")]
+        public string Metas()
+        {
+            return "Fixar todo o conteúdo, me aproximar do meu grupo de PI e atingir minhas metas de estudo por fora do horário do bootcamp";
         }
     }
 }
